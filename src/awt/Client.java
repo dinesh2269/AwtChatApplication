e8e852243583866bf7dcd2dee03c538eaeb84133
DineshK <DineshK@PENNANT-VSP119>
2024-05-23 10:46:17 +0530

    tretrt

--- src/awt/Client.java
+++ src/awt/Client.java
@@ -89,6 +89,7 @@ public class Client extends JFrame implements Runnable {
 		frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
 		frame.setVisible(true);
 		fram.add();
+		fram.add();
 	}
 
 	public void usersList() {
